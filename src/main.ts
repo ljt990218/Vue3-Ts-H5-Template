bf14d311d34069de6390fab0759dac984fbc1b02
ljt990218 <2511590093@qq.com>
2023-12-01 10:04:04 +0800

    updata: vant函数组件的样式问题

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ import App from './App.vue'
 import router from "./router";
 import i18n from './lang/index'
 
+import 'amfe-flexible'
 import '@/assets/css/reset.css'
 import '@/assets/css/index.css'
-import 'vant/lib/index.css';
-import 'amfe-flexible'
+
+// https://vant-contrib.gitee.io/vant/#/zh-CN/quickstart#4.-yin-ru-han-shu-zu-jian-de-yang-shi
+// Toast
+import { showToast } from 'vant';
+import 'vant/es/toast/style';
+// Dialog
+import 'vant/es/dialog/style';
 
 const pinia = createPinia()
 pinia.use(piniaPluginPersist)
